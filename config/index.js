586714f1228165f759c4582401589a75a6f462f1
goon1111 <112200467+goon1111@users.noreply.github.com>
2022-08-27 02:49:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,28 +1,23 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx0468a4b116bb5cc8",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "edcb0887848591645711273d94e5d5ef",
     // 模板消息id
-    templateId: "",
+    templateId: "pQVM6lkvR5Cx7roEQxNpviYG7QDz9nr8NWdDAEPs_x8",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: [],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "河北",
     // 所在城市
-    city: "惠州",
+    city: "邯郸",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+      {"name": "张有理", "year": "1999", "date": "01-06", "type": 'new'},
     ],
-    // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
     }
 
 // {{date.DATA}}  
@@ -30,8 +25,6 @@ export const config = {
 // 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
